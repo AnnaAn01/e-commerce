d0ea555388fe6fed6554c1d6009e3bb81807cace
Anna An <arzumanyan.an@gmail.com>
2022-06-03 11:44:21 +0400

    adding the Product component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Grid from '@material-ui/core';
+import React from "react";
+import Grid from "@material-ui/core";
+
+const products = [
+  { id: 1, name: "Shoes", description: "Running shoes." },
+  { id: 2, name: "Macbook", description: "Apple Macbook." },
+];
 
 const Products = () => {
-    <main>
-        <Grid container justify="center" spacing={4}>
-
+  <main>
+    <Grid container justify="center" spacing={4}>
+      {products.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+          <Product />
         </Grid>
-    </main>
-}
+      ))}
+    </Grid>
+  </main>;
+};
 
 export default Products;
